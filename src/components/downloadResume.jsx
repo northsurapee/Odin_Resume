61d8bc99aa5648f53316c1016ced14f7f81ea9d5
Surapee <suwan.srp@gmail.com>
2023-12-28 21:29:26 +0700

    Update downloadResume.jsx to import html2canvas

--- src/components/downloadResume.jsx
+++ src/components/downloadResume.jsx
@@ -1,20 +1,20 @@
-// import html2canvas from "html2canvas";
+import html2canvas from "html2canvas";
 import "../styles/downloadButton.css";
 
 export default function DownloadResume() {
-//   function handleDownloadResume() {
-//     const resumeElement = document.querySelector(".resume");
-//     html2canvas(resumeElement).then((canvas) => {
-//       const jpegDataUrl = canvas.toDataURL("image/jpeg");
-//       const link = document.createElement("a");
-//       link.href = jpegDataUrl;
-//       link.download = "resume.jpeg";
-//       link.click();
-//     });
-//   }
+  function handleDownloadResume() {
+    const resumeElement = document.querySelector(".resume");
+    html2canvas(resumeElement).then((canvas) => {
+      const jpegDataUrl = canvas.toDataURL("image/jpeg");
+      const link = document.createElement("a");
+      link.href = jpegDataUrl;
+      link.download = "resume.jpeg";
+      link.click();
+    });
+  }
 
   return (
-    <button className="download-resume" >
+    <button className="download-resume" onClick={handleDownloadResume}>
       <h3>
         <i className="fa-solid fa-arrow-up-right-from-square"></i>
         Download JPEG
